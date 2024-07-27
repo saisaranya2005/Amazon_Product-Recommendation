14cf34dd1490d7e875367174a0342a10fa832581
saisaranya2005 <139033511+saisaranya2005@users.noreply.github.com>
2024-07-27 22:18:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+import nltk
+from nltk.stem.snowball import SnowballStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from PIL import Image
+import streamlit as st
+
+# Download NLTK data
+nltk.download('punkt')
+
+# Load the dataset
+data = pd.read_csv("C:\\Users\\91630\\Downloads\\amazon_product.csv")
+
+# Remove unnecessary columns
+data = data.drop('id', axis=1)
+
+# Define tokenizer and stemmer
+stemmer = SnowballStemmer('english')
+def tokenize_and_stem(text):
+    tokens = nltk.word_tokenize(text.lower())
+    stems = [stemmer.stem(t) for t in tokens]
+    return stems
+
+# Create stemmed tokens column
+data['stemmed_tokens'] = data.apply(lambda row: tokenize_and_stem(row['Title'] + ' ' + row['Description']), axis=1)
+
+# Define TF-IDF vectorizer and cosine similarity function
+tfidf_vectorizer = TfidfVectorizer(tokenizer=tokenize_and_stem)
+def cosine_sim(text1, text2):
+    text1_concatenated = ' '.join(text1)
+    text2_concatenated = ' '.join(text2)
+    tfidf_matrix = tfidf_vectorizer.fit_transform([text1_concatenated, text2_concatenated])
+    return cosine_similarity(tfidf_matrix)[0][1]
+
+# Define search function
+def search_products(query):
+    query_stemmed = tokenize_and_stem(query)
+    data['similarity'] = data['stemmed_tokens'].apply(lambda x: cosine_sim(query_stemmed, x))
+    results = data.sort_values(by=['similarity'], ascending=False).head(10)[['Title', 'Description', 'Category']]
+    return results
+
+# Streamlit interface
+img = Image.open("c:\\Users\\91630\\Downloads\\groupphoto.jpg")
+st.image(img, width=600)
+st.title("Search Engine and Product Recommendation System ON Am Data")
+query = st.text_input("Enter Product Name")
+submit = st.button('Search')
+if submit:
+    res = search_products(query)
+    st.write(res)
